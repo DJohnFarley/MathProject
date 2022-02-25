5cfe8ce64544206968dce35000bc3f5bd4e5fa62
Daniel Farley <danieljohnfarley@yahoo.co.uk>
2022-02-25 21:11:59 +0000

    New functions to detect answer and user response

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // grab all equation buttons
-const equationBtns =  document.querySelectorAll(".equation-btn");
+const equationBtns = document.querySelectorAll(".equation-btn");
 
 //grab start button
 const start = document.getElementById('Start');
@@ -9,55 +9,76 @@ const int1 = document.getElementById("int1")
 const int2 = document.getElementById("int2")
 const sym = document.getElementById("symbol")
 
-
-
-
 let symArr = [];
 
-function equbuttons(arr){
+function equbuttons(arr) {
 
-    for(let i = 0 ; i < equationBtns.length; i++){
-        equationBtns[i].addEventListener('click', () =>{
-    
-            if(equationBtns[i].classList.contains("active")){
-                equationBtns[i].classList.remove('active')
-                arr.splice(equationBtns[i], 1);
-                
-            }
-    
-            else{
-                 equationBtns[i].classList.add("active")
+    for (let i = 0; i < equationBtns.length; i++) {
+        equationBtns[i].addEventListener('click', () => {
+
+            if (!equationBtns[i].classList.contains("active")) {
+                equationBtns[i].classList.add("active")
                 arr.push(equationBtns[i])
-            }; 
+            } else {
+                for (let j = 0; j <= arr.length; j++) {
+                    if (arr[j] == equationBtns[i]) {
+                        equationBtns[i].classList.remove("active")
+                        arr.splice(j, 1);
+                    };
+                };
+            };
         });
     };
+
     return arr
 }
+const buttonSelect = equbuttons(symArr);
 
-equbuttons(symArr);
-
- function startGame(arr){
-
+function startGame(arr) {
     start.addEventListener("click", () => {
+      
+        let numberOne = int1.innerHTML = Math.floor(Math.random() * 13);
+        let numberTwo = int2.innerHTML = Math.floor(Math.random() * 13);
+        let equationSymb;
+        let checkEs; 
 
-        console.log(symArr)
-        let numberOne = int1.innerHTML = Math.floor(Math.random() * 13)
-        let numberTwo =  int2.innerHTML = Math.floor(Math.random() * 13)
+        if (symArr.length > 0) {
+            equationSymb = arr[Math.floor(Math.random() * symArr.length)];
+            sym.innerHTML = equationSymb.innerHTML;
+           
+        } else {
 
-       if (symArr.length > 0 ){
-        const equationSymb =  sym.innerHTML = arr[Math.floor(Math.random() * symArr.length)].innerHTML;
-       }
-       else{
-        const equationSymb =  sym.innerHTML = ""
-        alert("Choose a symbol")
-       }
-             
-              
-        
+             sym.innerHTML = "";
+
+            alert("Choose a symbol");
+        }
+
+
+       let answer =  checkAnswer(numberOne, numberTwo, equationSymb);
+       console.log(answer);
     })
- }
+}
+
+startGame(buttonSelect);
 
 
- startGame(symArr);
+function checkAnswer(num1, num2, es){
+
+    switch(es.id){
+        case "times":
+            return num1 * num2 ;
+            break;
+        case "divide":
+            return num1 / num2 ;
+            break;
+        case "addition":
+            return num1 + num2 ;
+            break;
+        case "minus":
+            return num1 - num2;
+    }; 
+
+
+}
 
 
