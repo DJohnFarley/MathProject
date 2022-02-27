28b5d984894882598c37cdd7712fa36528a834dc
Daniel Farley <danieljohnfarley@yahoo.co.uk>
2022-02-27 14:34:55 +0000

    All math and checking between inputs have now been finished, I have begun designing the interface.

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const equationBtns = document.querySelectorAll(".equation-btn");
 //grab start button
 const start = document.getElementById('Start');
 
+//answers button 
+const answerValue = document.getElementById('answer');
+const answerMessage = document.getElementById('answerMessage');
+const answerButton = document.getElementById('checkbtn');
+
 //grab integer value containers 
 const int1 = document.getElementById("int1")
 const int2 = document.getElementById("int2")
 const sym = document.getElementById("symbol")
 
+// declare my integer, equation symbol & answer;
 let symArr = [];
+let numberOne;
+let numberTwo;
+let equationSymb;
+let answer;
 
 function equbuttons(arr) {
 
@@ -36,49 +46,67 @@ const buttonSelect = equbuttons(symArr);
 
 function startGame(arr) {
     start.addEventListener("click", () => {
-      
-        let numberOne = int1.innerHTML = Math.floor(Math.random() * 13);
-        let numberTwo = int2.innerHTML = Math.floor(Math.random() * 13);
-        let equationSymb;
-        let checkEs; 
+
+        numberOne = int1.innerHTML = Math.floor(Math.random() * 13);
+        numberTwo = int2.innerHTML = Math.floor(Math.random() * 13);
+        equationSymb;
+
 
         if (symArr.length > 0) {
             equationSymb = arr[Math.floor(Math.random() * symArr.length)];
             sym.innerHTML = equationSymb.innerHTML;
-           
+
         } else {
 
-             sym.innerHTML = "";
+            sym.innerHTML = "";
 
             alert("Choose a symbol");
         }
 
+        answer = makeEquation(numberOne, numberTwo, equationSymb)
+      
+        console.log(answer)
 
-       let answer =  checkAnswer(numberOne, numberTwo, equationSymb);
-       console.log(answer);
-    })
-}
+    });
+};
 
 startGame(buttonSelect);
 
+function makeEquation(num1, num2, es) {
 
-function checkAnswer(num1, num2, es){
-
-    switch(es.id){
+    switch (es.id) {
         case "times":
-            return num1 * num2 ;
+            return num1 * num2;
             break;
         case "divide":
-            return num1 / num2 ;
+            return num1 / num2;
             break;
         case "addition":
-            return num1 + num2 ;
+            return num1 + num2;
             break;
         case "minus":
             return num1 - num2;
-    }; 
+    };
+};
+
+checkAnswer();
+
+function checkAnswer() {
+
+    answerButton.addEventListener('click', () => {
+
+        let userInput = answerValue.value;
+
+        console.log(answer)
+        console.log(userInput)
 
 
+        if (userInput == answer) {
+            answerMessage.innerText = 'well done';
+
+        } else {
+            answerMessage.innerText = 'Close.. try again!';
+        }
+
+    })
 }
-
-
